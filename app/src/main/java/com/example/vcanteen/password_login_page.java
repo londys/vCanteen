d03923a1ded77e7c5474b066d48f29786a8af1ef
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2019-03-18 15:59:17 +0700

    loginv1

--- app/src/main/java/com/example/vcanteen/password_login_page.java
+++ app/src/main/java/com/example/vcanteen/password_login_page.java
@@ -1,18 +1,38 @@
 package com.example.vcanteen;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class password_login_page extends AppCompatActivity {
 
     private String email;
+    private String passwd;
+    private String account_type = "NORMAL";
     private EditText passwdField;
     private Button showBtn;
+    private ImageButton next;
+    private boolean isHidden = true;
+
+    private final String dbAddress = "https://en33remma22tb.x.pipedream.net/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +54,89 @@ public class password_login_page extends AppCompatActivity {
 
         passwdField = findViewById(R.id.passwordBox);
         showBtn = findViewById(R.id.show_pw_btn);
+        next = findViewById(R.id.next_button);
 
         System.out.println(passwdField.getText().toString());
 
         showBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(passwdField.getInputType());
-                System.out.println(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                System.out.println(InputType.TYPE_NULL);
-                if (passwdField.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
-                    passwdField.setInputType(InputType.TYPE_NULL);
-                    System.out.println("set to null");
+                if (isHidden) {
+                    passwdField.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    isHidden = false;
                 } else {
-                    passwdField.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    System.out.println("set to passwd");
+                    passwdField.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    isHidden = true;
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(password_login_page.this, homev1Activity.class);
+                passwd = passwdField.getText().toString();
+                JSONObject postData = new JSONObject();
+                try {
+                    postData.put("account_type", account_type);
+                    postData.put("email", email);
+                    postData.put("passwd", passwd == null ? JSONObject.NULL : passwd);
+                    System.out.println(postData.toString());
+                    (new SendDeviceDetails()).execute(dbAddress, postData.toString());
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Connection Error", Toast.LENGTH_SHORT);
                 }
             }
         });
     }
 
+    private class SendDeviceDetails extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String data = "";
+
+            HttpURLConnection httpURLConnection = null;
+            try {
+
+                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+                wr.writeBytes("PostData=" + params[1]);
+                wr.flush();
+                wr.close();
+
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+                int inputStreamData = inputStreamReader.read();
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    data += current;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
+        }
+    }
     /*@Override
     public void finish() {
          super.finish();
