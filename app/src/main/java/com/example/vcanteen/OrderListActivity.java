dacf47256bfce0d0b705ad3ad2a50f3b7da38f1d
tannueng <tannueng@gmail.com>
2019-04-02 02:00:14 +0700

    additional files to make app work
    from fake json

--- app/src/main/java/com/example/vcanteen/OrderListActivity.java
+++ app/src/main/java/com/example/vcanteen/OrderListActivity.java
@@ -1,13 +1,48 @@
 package com.example.vcanteen;
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderListActivity extends AppCompatActivity {
+    boolean tab = true;
+    boolean pressed = false;
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new progressTabFragment(), "IN PROGRESS");
+        adapter.addFragment(new historyTabFragment(), "HISTORY");
+        viewPager.setAdapter(adapter);
+    }
+
 }
