071a9fcc0135e329d0a83323d5aad70848844b08
Patt Kosit <pattabhum_patt@hotmail.com>
2019-04-05 11:26:20 +0700

    clean before merge

--- app/src/main/java/com/example/vcanteen/homev1Activity.java
+++ app/src/main/java/com/example/vcanteen/homev1Activity.java
@@ -1,17 +1,42 @@
 package com.example.vcanteen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.internal.LockOnGetVariable;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class homev1Activity extends AppCompatActivity {
 
+    private List<vendorList> vendorLists;
     private TextView mTextMessage;
+    private ListView listView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,15 +44,22 @@ public class homev1Activity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+
+                //remove the comments and edit class name
+
+
                 case R.id.navigation_profile:
-                    mTextMessage.setText("PROFILE");
-                    return true;
+                    //Intent intent = new Intent(homev1Activity.this, profile class here.class);
+                    //startActivity(intent);
+                    break;
                 case R.id.navigation_orders:
-                    mTextMessage.setText("ORDERS");
-                    return true;
+                    //Intent intent = new Intent(homev1Activity.this, orders class here.class);
+                    //startActivity(intent);
+                    break;
                 case R.id.navigation_settings:
-                    mTextMessage.setText("SETTINGS");
-                    return true;
+                    //Intent intent = new Intent(homev1Activity.this, setting class here.class);
+                    //startActivity(intent);
+                    break;
             }
             return false;
         }
@@ -39,13 +71,115 @@ public class homev1Activity extends AppCompatActivity {
         setContentView(R.layout.activity_home_v1);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        listView = findViewById(R.id.vendorlist);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        String[] test = {"ESAN food","Fried Chicken with Sticky Rice","Food3","Food4","Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Food6", "Food 77"};
+       /* String[] test = {"ESAN food","Fried Chicken with Sticky Rice","Food3","Food4","Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Food6", "Food 77"};
         ListAdapter testAdapter = new vendorListAdapter(this, test);
         ListView vendorList = findViewById(R.id.vendorlist);
-        vendorList.setAdapter(testAdapter);
+        vendorList.setAdapter(testAdapter);*/
+        getVendorList();
+
+
+
     }
-    
+
+    private void getVendorList(){
+
+        String url = "https://vcanteen.herokuapp.com/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        vendorListApi jsonPlaceHolderApi = retrofit.create(vendorListApi.class);
+
+        Call<List<vendorList>> call = jsonPlaceHolderApi.getVendorList();
+
+        call.enqueue(new Callback<List<vendorList>>() {
+            @Override
+            public void onResponse(Call<List<vendorList>> call, Response<List<vendorList>> response) {
+
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),"cannot connect error code: "+response.code(),Toast.LENGTH_LONG).show();
+                    //System.out.println("\n\n\n\n********************"+ "Code: " + response.code() +"********************\n\n\n\n");
+                    return;
+                }
+                //Log.w("gson response :",new Gson().toJson(response.body().getRestaurantName()));
+               // vendorLists = (List<vendorList>) response.body();
+
+                //listView.setAdapter(new vendorListAdapter(getApplicationContext(), vendorLists));
+
+                List<vendorList> vendorLists = response.body();
+                Log.d("TEST", String.valueOf(vendorLists.size()));
+                ArrayList<vendorList> temp = new ArrayList<vendorList>();
+                //for(vendorList v: vendorLists){
+                    for(int i =0; i<vendorLists.size();i++){
+                   // Log.d("vendor name: ",v.getRestaurantName());
+                    /*int vendorId = v.getVendorId();
+                    String vendorName = v.getRestaurantName();
+                    int vendorNumber = v.getRestaurantNumber();
+                    String vendorImageURL = v.getVendorImage();
+                    String vendorStatus = v.getVendorStatus();*/
+
+                    int vendorId = vendorLists.get(i).getVendorId();
+                    String vendorName = vendorLists.get(i).getRestaurantName();
+                    int vendorNumber = vendorLists.get(i).getRestaurantNumber();
+                    String vendorImageURL = vendorLists.get(i).getVendorImage();
+                    String vendorStatus = vendorLists.get(i).getVendorStatus();
+
+
+                    vendorList newVendorList = new vendorList(vendorId,vendorName,vendorNumber,vendorImageURL,vendorStatus);
+                    temp.add(newVendorList);
+                   /* if(vendorStatus=="CLOSED") {
+                        temp.add(vendorLists.size(),newVendorList);
+                    }else{
+                        temp.add(newVendorList);
+                    }*/
+                }
+                vendorListAdapter adapter = new vendorListAdapter(homev1Activity.this,temp);
+
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<vendorList>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        /*call.enqueue(new Callback<vendorList>() {
+            @Override
+            public void onResponse(Call<vendorList> call, Response<vendorList> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),"cannot connect error code: "+response.code(),Toast.LENGTH_LONG).show();
+                    //System.out.println("\n\n\n\n********************"+ "Code: " + response.code() +"********************\n\n\n\n");
+                    return;
+                }
+                Log.w("gson response :",new Gson().toJson(response.body().getRestaurantName()));
+                    vendorLists = (List<vendorList>) response.body();
+
+                listView.setAdapter(new vendorListAdapter(getApplicationContext(), vendorLists));
+
+            }
+
+            @Override
+            public void onFailure(Call<vendorList> call, Throwable t) {
+
+            }
+        });*/
+    }
+    public boolean isEnabled(String status) {
+        if (status == "CLOSED") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
--- app/src/main/java/com/example/vcanteen/vendorListAdapter.java
+++ app/src/main/java/com/example/vcanteen/vendorListAdapter.java
@@ -1,29 +1,70 @@
 package com.example.vcanteen;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class vendorListAdapter extends ArrayAdapter {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
-    vendorListAdapter(Context context, String[] a){
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-        super(context, R.layout.vendor_listview , a);
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class vendorListAdapter extends ArrayAdapter<vendorList> {
+    RequestOptions option = new RequestOptions().centerCrop();
+    String vendorStatus;
+
+    Context context;
+    int resource;
+    List<vendorList> vendorLists = null;
+
+    public vendorListAdapter(Context context, List<vendorList> vendorLists) {
+
+        super(context, R.layout.vendor_listview, vendorLists);
+        this.context = context;
+        this.vendorLists = vendorLists;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        LayoutInflater vendorListInflater = LayoutInflater.from(getContext());
-        View customView = vendorListInflater.inflate(R.layout.vendor_listview, parent, false);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        vendorList item = vendorLists.get(position);
+        vendorStatus = item.getVendorStatus();
+            if(vendorStatus.equals("CLOSED")){
+                convertView = LayoutInflater.from(context).inflate(R.layout.vendor_listview_disabled, parent, false);
+            } else
+            convertView = LayoutInflater.from(context).inflate(R.layout.vendor_listview, parent, false);
 
-        String singleItem = (String) getItem(position);
-        TextView vendorname = (TextView) customView.findViewById(R.id.vendorName);
 
-        vendorname.setText(singleItem);
-        return customView;
+        TextView vendorName = (TextView) convertView.findViewById(R.id.vendorName);
+        ImageView vendorImage = convertView.findViewById(R.id.vendorImage);
+
+        vendorName.setText(item.getRestaurantName());
+        //vendorImage.setImageBitmap(loadBitmap(item.getVendorImage()));
+        Glide.with(context).load(item.getVendorImage()).apply(option).into(vendorImage);
+        vendorImage.setClipToOutline(true);
+
+
+        return convertView;
     }
+
 }
+
 
